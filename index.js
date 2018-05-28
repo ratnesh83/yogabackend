d0775069d58a6453688008a959cd22af4f0518c4
Ratnesh Singh <ratnesh_singh@outlook.com>
2018-05-28 14:36:07 +0530

    abc

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const db = require('./dao');
 
 var app = express();
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 
 db.connect();
